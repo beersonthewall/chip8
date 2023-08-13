dc4b7f3d729efeb0e157ab30e9bcecf44527e3e5
Matthew <matthew@beersonthewall.com>
2023-08-13 08:54:59 -0500

    Chip 8: adds css so the site looks real good!

--- src/Chip8/Controls.js
+++ src/Chip8/Controls.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './Controls.css';
+import FolderIcon from './folder-open-solid.svg';
 
 export default function Controls({ loadProgram, reset }) {
     return (
-	<div className="ctrlContainer">
-	    <button onClick={reset}>Reset</button>
-	    <form onSubmit={loadProgram}>
-		<input type="file" name="file" />
-		<input type="submit" value="Run Program" />
-	    </form>
-	</div>
+	<form onSubmit={loadProgram} className="ctrlContainer">
+	    <button className="ctrl" onClick={reset}>Reset</button>
+	    <label className="romUploadLbl ctrl" for="romUpload" >
+		<img src={FolderIcon} id="folderImg" />
+		Load ROM
+	    </label>
+	    <input id="romUpload" className="ctrl" type="file" name="file" />
+	    <input className="ctrl run" type="submit" value="Run Program" />
+	</form>
     );
 }
