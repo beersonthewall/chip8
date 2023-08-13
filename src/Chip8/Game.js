710625c7bc68dfe34da936dcfa4933bd76223cad
Matthew <matthew@beersonthewall.com>
2023-08-12 21:58:45 -0500

    Game: remove useEffect, and enable stopping the interpreter.

--- src/Chip8/Game.js
+++ src/Chip8/Game.js
@@ -4,41 +4,21 @@ import './Game.css';
 import Controls from './Controls';
 
 let interp = new Interpreter();
+let interval = null;
 
 export default function Game({ height, width }) {
     const canvasRef = useRef(null);
 
     const keydown = (e) => {
 	interp.pressKey(e.key);
-	e.preventDefault();
     };
     const keyup = (e) => {
 	interp.liftKey(e.key);
-	e.preventDefault();
     };
 
     document.addEventListener('keydown', keydown);
     document.addEventListener('keyup', keyup);
-    
-    useEffect(() => {
-	const ctx = canvasRef.current?.getContext("2d");
-	if(ctx != null) {
-	    const frameTime = 1000/60;
-	    let last = Date.now();
-	    let origin = last + frameTime/2;
-	    setInterval(_ => {
-		last += (Date.now()-last);
-		if(interp.hlt) return;
-		for(let k = 0; origin < last-frameTime && k < 2; origin+=frameTime,k++) {
-		    let tickrate = 7;
-		    for(let z = 0; z < tickrate && !interp.waiting; z++) {
-			interp.tick(ctx);
-		    }
-		    interp.timerTick();
-		}
-	    }, frameTime);
-	}
-    });
+
 
     function loadProgram(e) {
 	e.preventDefault();
@@ -50,6 +30,22 @@ export default function Game({ height, width }) {
 		interp.reset(ctx);
 	    }
 	    interp.loadProgram(new Uint8Array(reader.result));
+	    if(ctx != null) {
+		const frameTime = 1000/60;
+		let last = Date.now();
+		let origin = last + frameTime/2;
+		interval = setInterval(_ => {
+		    last += (Date.now()-last);
+		    if(interp.hlt) return;
+		    for(let k = 0; origin < last-frameTime && k < 2; origin+=frameTime,k++) {
+			let tickrate = 7;
+			for(let z = 0; z < tickrate && !interp.waiting; z++) {
+			    interp.tick(ctx);
+			}
+			interp.timerTick();
+		    }
+		}, frameTime);
+	    }
 	};
 	reader.readAsArrayBuffer(file);
     }
@@ -58,6 +54,10 @@ export default function Game({ height, width }) {
 	e.preventDefault();
 	const ctx = canvasRef.current?.getContext("2d");
 	interp.reset(ctx);
+	if(interval) {
+	    clearInterval(interval);
+	    interval = null;
+	}
     }
 
     return (
